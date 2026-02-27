15e10807a09397c43049f43cf4dda594bc156685
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-27 16:36:01 +0530

    use loop

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,50 @@ void setup() {
     pinMode(green_led, OUTPUT);
 }
 
+void loop() {
+
+    // 🌿 GREEN PULSE
+    digitalWrite(red_led, LOW);
+    digitalWrite(blue_led, LOW);
+    digitalWrite(green_led, HIGH);
+    delay(800);
+
+    // 💙 BLUE FLASH
+    digitalWrite(red_led, LOW);
+    digitalWrite(blue_led, HIGH);
+    digitalWrite(green_led, LOW);
+    delay(800);
+
+    // ❤️ RED GLOW
+    digitalWrite(red_led, HIGH);
+    digitalWrite(blue_led, LOW);
+    digitalWrite(green_led, LOW);
+    delay(800);
+
+    // 💜 RED + BLUE (MAGENTA EFFECT)
+    digitalWrite(red_led, HIGH);
+    digitalWrite(blue_led, HIGH);
+    digitalWrite(green_led, LOW);
+    delay(800);
+
+    // 💛 RED + GREEN (YELLOW EFFECT)
+    digitalWrite(red_led, HIGH);
+    digitalWrite(blue_led, LOW);
+    digitalWrite(green_led, HIGH);
+    delay(800);
+
+    // 🩵 BLUE + GREEN (CYAN EFFECT)
+    digitalWrite(red_led, LOW);
+    digitalWrite(blue_led, HIGH);
+    digitalWrite(green_led, HIGH);
+    delay(800);
+
+    // 🌈 ALL COLORS ON (WHITE BLAST)
+    digitalWrite(red_led, HIGH);
+    digitalWrite(blue_led, HIGH);
+    digitalWrite(green_led, HIGH);
+    delay(1000);
+
+    // ⚫ ALL OFF (RESET PAUSE)
+
+}
