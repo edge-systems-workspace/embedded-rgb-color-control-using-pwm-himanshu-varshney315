1bdfe23e3d5df2f8b05994965a496ae72821643e
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-27 16:34:46 +0530

    update author name and date

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,6 @@
-#include <Arduino.h>
-/**
- * @file main.cpp
- * @brief Embedded RGB LED Control (Digital + PWM)
- * @author himanshu gupta
- * @date 2026-02-20
- *
- * @details
- * Controls RGB LED using digital ON/OFF
- * and analog PWM brightness control.
- */
+#incude <Arduino.h>
 
-int led_pin=2;
+int red_led = 10;
+int blue_led = 11;
+int green_led = 9;
 
-void setup() {
-    pinMode(led_pin, OUTPUT);
-}
-
-void loop() {
-    digitalWrite(led_pin,HIGH);
-    delay(30);
-    digitalWrite(led_pin,LOW);
-    delay(10);
-}
